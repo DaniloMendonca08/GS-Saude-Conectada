2a60408fcbd8769e53440d5843f3c3079b4822c6
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-16 18:06:36 -0300

    pastas-style-assets

---
+++ src/styles/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+    primary: {
+        blue: "#292F93",
+        orange: "#FF8100",
+        cream: "#ffe6cc",
+    }
+}
---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+
+const styled = { createGlobalStyle }
+
+const GlobalStyles = styled.createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+html,
+body {
+    min-height: 100vh;
+    max-width: 100vw;
+    overflow-x: hidden;
+}
+`
+
+export default GlobalStyles
