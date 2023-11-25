1c4e91fa5fe654452e84718042c15e73825bef9a
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-24 23:05:55 -0300

    page-Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import Image from 'next/image'
-import Menu from './components/Menu/Menu'
-import { Header } from './components/Header/Header'
+"use client"
+
+import Cadastro from './cadastro/page'
+import BodyHome from './components/BodyHome/BodyHome'
+import BodyPagPrincipal from './components/BodyPagPrincipal/BodyPagPrincipal'
+import Erro404 from './error/404/page'
+import Login from './login/page'
 
 export default function Home() {
   return (
-    <main>
-      <Header />
-    </main>
+    <>
+    <BodyHome />
+    </>
   )
 }
