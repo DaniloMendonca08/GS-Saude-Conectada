4fc1e0d92c2f80f6140c5b54b3df95c189d62fa3
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-24 23:04:12 -0300

    create-page-AttLogin

---
+++ src/app/AttLogin/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import BodyAttLogin from "../components/BodyAttLogin/BodyAttLogin"
+
+const AttLogin = () => {
+    return (
+        <BodyAttLogin />
+    )
+}
+
+export default AttLogin
