2c5d74f5db88ed23de0778ad3a16ece76c330ded
Danilo Mendonça <danilomendonca08@gmail.com>
2023-11-24 23:06:12 -0300

    finish-layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,20 +3,25 @@
 import GlobalStyles from '@/styles/globalStyles'
 import { Header } from './components/Header/Header'
 import { Footer } from './components/Footer/Footer'
+import { StyleSheetManager } from 'styled-components'
+import isPropValid from "@emotion/is-prop-valid"
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <html lang="pt-BR">
+      <StyleSheetManager shouldForwardProp={prop => isPropValid(prop)}>
       <GlobalStyles />
       <body>
         <Header />
         {children}
         <Footer />
       </body>
+      </StyleSheetManager>
     </html>
   )
 }
